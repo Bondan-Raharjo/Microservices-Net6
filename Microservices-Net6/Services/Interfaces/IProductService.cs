baeaceb60cd7a76977a2bca30cd587d47e2dcde8
Bondan Raharjo <bondanraharjo66@gmail.com>
2025-04-17 09:26:48 +0700

    updated interface service

---
+++ Microservices-Net6/Services/Interfaces/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microservices_Net6.DTOs;
+using Microservices_Net6.Models;
+using Microservices_Net6.Repositories.Interfaces;
+
+namespace Microservices_Net6.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAllAsync();
+        Task<ProductDTO> GetByIdAsync(int id);
+        Task<IEnumerable<ProductDTO>> SearchByNameAsync(string name);
+        Task<IEnumerable<ProductDTO>> SearchByPriceRangeAsync(decimal minPrice, decimal maxPrice);
+        Task<ProductDTO> CreateAsync(CreateProductDTO productDto);
+        Task<ProductDTO> UpdateAsync(int id, UpdateProductDTO productDto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Microservices-Net6/Services/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microservices_Net6.DTOs;
+using Microservices_Net6.Models;
+using Microservices_Net6.Repositories.Interfaces;
+
+namespace Microservices_Net6.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<AuthResponseDTO> RegisterAsync(RegisterRequestDTO request);
+        Task<AuthResponseDTO> LoginAsync(LoginRequestDTO request);
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByEmailAsync(string email);
+    }
+
+}
